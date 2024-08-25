56e721a2806a8cee042b43be225d426583388ea4
Majbrit Schöttner <majbrit.schoettner@yahoo.de>
2024-08-25 20:50:38 +0200

    multi step register weiter

--- JavaScriptDeveloper/Multi-stepRegister/js/script.js
+++ JavaScriptDeveloper/Multi-stepRegister/js/script.js
@@ -1,12 +1,30 @@
 var currentStep = 0;
 showStep(currentStep);
 
+var name, email;
+var topics; 
+
 function showStep(n) {
-    var x = document.getElementsByClassName("step");
-    x[n].style.display = "block";
-    var c = document.getElementsByClassName("step-count");
-    c[n].style.background = "#845EEE";
-
-
-    fixStepIndicator(n)
+  var steps = document.getElementsByClassName("step");
+  if(steps[n-1]){
+    steps[n-1].style.display = "none";
   }
+  steps[n].style.display = "flex";
+
+  var counts = document.getElementsByClassName("step-count");
+  counts[n].style.background = "#652CD1";
+  counts[n].style.border = "5px solid #652CD1";
+
+  document.getElementById("count-num").innerHTML = n+1;
+}
+
+function continueStep() {
+  if (!validateForm()) 
+    return false;
+  currentStep++;
+  showStep(currentStep);
+}
+
+function validateForm() {
+  return true;
+}
