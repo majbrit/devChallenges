9b7c6ae8a1b27b7515570c0146ff2f672dc725a8
Majbrit Schöttner <majbrit.schoettner@yahoo.de>
2024-08-26 15:27:59 +0200

    step 1 js step 2 css

--- JavaScriptDeveloper/Multi-stepRegister/js/script.js
+++ JavaScriptDeveloper/Multi-stepRegister/js/script.js
@@ -1,19 +1,23 @@
-var currentStep = 0;
+var currentStep = 1;// 0
 showStep(currentStep);
 
-var name, email;
+var namefl, email;
 var topics; 
 
 function showStep(n) {
-  var steps = document.getElementsByClassName("step");
-  if(steps[n-1]){
-    steps[n-1].style.display = "none";
-  }
+  var steps = document.getElementsByClassName("step");  
   steps[n].style.display = "flex";
 
   var counts = document.getElementsByClassName("step-count");
   counts[n].style.background = "#652CD1";
-  counts[n].style.border = "5px solid #652CD1";
+  
+  var stepShadow = document.getElementsByClassName("step-shadow");
+  stepShadow[n].style.background = "rgba(132, 94, 238, 0.3)";
+
+  if(steps[n-1]){
+    steps[n-1].style.display = "none";
+    stepShadow[n-1].style.background = "transparent";
+  }
 
   document.getElementById("count-num").innerHTML = n+1;
 }
@@ -26,5 +30,33 @@ function continueStep() {
 }
 
 function validateForm() {
-  return true;
+  
+  if(currentStep == 0) {
+    email = document.getElementById("email");
+    namefl = document.getElementById("name");
+    emailValidation = validateInput(email, /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/); 
+    nameValidation = validateInput(namefl, /^[\u00C0-\u017Fa-zA-Z, ]+$/);
+    if(emailValidation && nameValidation) {
+      return true;
+    } else {
+      return false;
+    }
+  } else if (currentStep == 1) {
+    return true;
+  }
+  
+}
+
+function confirmStep() {
+  alert("\u2705 Success");
+}
+
+function validateInput(input, validRegex) {
+  if (input.value.match(validRegex)) {
+    input.style.boxShadow = "0px 0px 0px transparent";
+    return true;
+  } else {
+    input.style.boxShadow = "0px 0px 6px #d12c60";
+    return false;
+  }
 }
