2625b2b7b372d6fa4fd7f7c225be994b53d822b8
Majbrit Schöttner <majbrit.schoettner@yahoo.de>
2025-07-16 16:50:11 +0200

    .

--- FullstackDeveloper/MyTaskBoard/taskboard/src/app/app.routes.ts
+++ FullstackDeveloper/MyTaskBoard/taskboard/src/app/app.routes.ts
@@ -5,6 +5,6 @@ import { BoardComponent } from './components/board/board.component';
 export const routes: Routes = [
     { path: 'board/:boardId', component: BoardComponent }, 
     { path: '', redirectTo: '/board/new', pathMatch: 'full' }, // Umleitung zu /board
-  { path: '**', redirectTo: '/board/new' } 
+    { path: '**', redirectTo: '/board/new' } 
 
 ];
